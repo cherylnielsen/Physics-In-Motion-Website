6f147626e17521a2e69724711e90f0848155b634
Cheryl R. Nielsen <cnielsen@mail.sfsu.edu>
2019-02-22 21:49:58 -0800

    added views to database, and merged homework & submission tables

--- private/model/homework.php
+++ private/model/homework.php
@@ -1,23 +1,33 @@
 <?php
 
+
 class Homework {
 	
 	private $homework_id; // key 
+	private $section_id;
+	private $assignment_id; 
 	private $student_id;		
-	private $assignment_id; 	
+	// the homework produced
 	private $lab_summary;
 	private $lab_data;
 	private $graphs;
 	private $math;
 	private $hints;
 	private $chat_session;	
-	
-		
+	// the homework submitted
+	private $date_submitted;
+	private $points_earned;
+	private $was_graded;
+	private $hours;
+			
 	public function __construct() {}
 	
-	public function initialize($assignment_id, $student_id, $lab_summary, $lab_data, 
-								$graphs, $math, $hints, $chat_session)
+	public function initialize($homework_id, $section_id, $assignment_id, $student_id, 
+								$lab_summary, $lab_data, $graphs, $math, $hints, $chat_session,
+								$date_submitted, $points_earned = 0, $was_graded = false, $hours = 0)
 	{
+		$this->homework_id = $homework_id;
+		$this->section_id = $section_id;
 		$this->assignment_id = $assignment_id;
 		$this->student_id = $student_id;
 		$this->lab_summary = $lab_summary;
@@ -26,8 +36,31 @@ class Homework {
 		$this->math = $math;
 		$this->hints = $hints;
 		$this->chat_session = $chat_session;
+		$this->date_submitted = $date_submitted;
+		$this->points_earned = $points_earned;
+		$this->hours = $hours;
+		$this->was_graded = $was_graded;	
 	}
 	
+	public function get_homework_id()
+	{
+		return $this->homework_id;
+	}
+
+	public function set_homework_id($homework_id)
+	{
+		$this->homework_id = $homework_id;
+	}
+	
+	public function get_section_id()
+	{
+		return $this->section_id;
+	}
+
+	public function set_section_id($section_id)
+	{
+		$this->section_id = $section_id;
+	}	
 	
 	public function get_assignment_id()
 	{
@@ -119,6 +152,45 @@ class Homework {
 		$this->chat_session = $chat_session;
 	}
 
+	public function get_date_submitted()
+	{
+		return $this->date_submitted;
+	}
+
+	public function set_date_submitted($date_submitted)
+	{
+		$this->date_submitted = $date_submitted;
+	}
+	
+	public function get_points_earned()
+	{
+		return $this->points_earned;
+	}
+
+	public function set_points_earned($points_earned)
+	{
+		$this->points_earned = $points_earned;
+	}
+	
+	public function get_hours()
+	{
+		return $this->hours;
+	}
+
+	public function set_hours($hours)
+	{
+		$this->hours = $hours;
+	}
+	
+	public function get_was_graded()
+	{
+		return $this->was_graded;
+	}
+
+	public function set_was_graded($was_graded)
+	{
+		$this->was_graded = $was_graded;
+	}
 	
 }
 
